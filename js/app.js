db4238f4e7b907542bf67acded55a179f4ee6345
hatemsallam <hatemsallam94@hotmail.com>
2021-05-25 05:53:39 +0300

    pushing lab 02

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict' ;
+let userName=prompt('what\'s your name ?')
+alert('welcome to our site '+userName)
+
+let age= prompt('my age is 26 yes or no ?')
+age= age.toLowerCase();
+if (age=='yes') {alert('that\'s true') }
+else {alert('that\'s false')}
+
+//console.log('my age is 26')
+
+let food= prompt('my favourite food is mansaf yes or no ?')
+food= food.toLowerCase();
+if (food=='yes') {alert('that\'s true') }
+else {alert('that\'s false')}
+
+//console.log('my favourite food is mansaf')
+
+let hobbie=prompt('my favourite hobbie is playing cards yes or no ?')
+hobbie= hobbie.toLowerCase();
+if (hobbie=='yes') {alert('that\'s true') }
+else {alert('that\'s false')}
+
+//console.log('my favourite hobbie is playing cards')
+
+let team=prompt('my favourite team is Real madrid yes or no ?')
+team= team.toLowerCase();
+if (team=='no') {alert('that\'s true my favourite team is barcelona') }
+else {alert('that\'s false my favourite team is barcelona')}
+
+//console.log('my favourite team is barcelona')
+
+let movie=prompt('my favourite movie is sherlock holmes yes or no ?')
+movie= movie.toLowerCase();
+if (movie=='yes') {alert('that\'s true') }
+else {alert('that\'s false')}
+
+//console.log('my favourite movie is sherlock holmes')
+
+alert('thank you for visiting our site '+userName)
