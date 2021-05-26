31b76600b4f839911daea0defdabebe7bd75c679
Mohammad Harb <md20.harb21@gmail.com>
2021-05-26 16:16:29 +0300

    adding function 1

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
 'use strict';
+
 let score=0;
 let userName=prompt('what\'s your name ?');
-alert('welcome to our site '+userName);
-let age= prompt('my age is 26 yes or no ?');
-age= age.toLowerCase();
-if (age==='yes') {alert('that\'s true'); score++; }
-else {alert('that\'s false');}
+
+function question1(){
+
+  alert('welcome to our site '+userName);
+  let age= prompt('my age is 26 yes or no ?');
+  age= age.toLowerCase();
+  if (age==='yes') {alert('that\'s true'); score++; }
+  else {alert('that\'s false');}
 //console.log('my age is 26')
+}
+question1();
+
+
 let food= prompt('my favourite food is mansaf yes or no ?');
 food= food.toLowerCase();
 if (food==='yes') {alert('that\'s true'); score++; }
 else {alert('that\'s false');}
 //console.log('my favourite food is mansaf')
+
 let hobbie=prompt('my favourite hobbie is playing cards yes or no ?');
 hobbie= hobbie.toLowerCase();
 if (hobbie==='yes') {alert('that\'s true'); score++; }
 else {alert('that\'s false');}
 //console.log('my favourite hobbie is playing cards')
+
 let team=prompt('my favourite team is Real madrid yes or no ?');
 team= team.toLowerCase();
 if (team==='no') {alert('that\'s true my favourite team is barcelona'); score++; }
 else {alert('that\'s false my favourite team is barcelona');}
 //console.log('my favourite team is barcelona')
+
 let movie=prompt('my favourite movie is sherlock holmes yes or no ?');
 movie= movie.toLowerCase();
 if (movie==='yes') {alert('that\'s true'); score++; }
 else {alert('that\'s false');}
 //console.log('my favourite movie is sherlock holmes')
+
 for (let i = 0; i < 4; i++) {
   let number=prompt('guess a number between 0 and 10') ;
   if (number<=5) {alert('too low!'); }else if (number>=9) {alert('too high!');}
@@ -38,6 +50,8 @@ for (let i = 0; i < 4; i++) {
     alert('the answer is 7');
   }
 }
+
+
 let typesOffood='';
 for (let i = 0; i <6; i++){
   typesOffood=['mansaf','shawerma','burger','fried chicken'];
@@ -56,5 +70,7 @@ for (let i = 0; i <6; i++){
     alert('Wrong!');
   }
 }
+
+
 alert('thank you for visiting our site '+userName);
 alert (`Your score is ${score} /7`);
